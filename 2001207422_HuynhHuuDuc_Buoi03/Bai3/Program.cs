69047acd88f00999b60cefd792ca30ee2cef6420
A209PC28\Administrator <huynhhuuduc5031@gmail.com>
2023-08-30 15:48:07 +0700

    new bai3

---
+++ 2001207422_HuynhHuuDuc_Buoi03/Bai3/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bai3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TamGiac tg = new TamGiac();
+            tg.Ma = 2;
+            tg.Mb = 2;
+            tg.Mc = 2;
+            Console.WriteLine("CV:{0} DT:{1}", tg.CV(), tg.DT());
+            tg.KiemTra();
+            Console.ReadLine();
+        }
+    }
+}
---
+++ 2001207422_HuynhHuuDuc_Buoi03/Bai3/TamGiac.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bai3
+{
+    class TamGiac
+    {
+        private int ma, mb, mc;
+
+        public int Mc
+        {
+            get { return mc; }
+            set { mc = value; }
+        }
+
+        public int Mb
+        {
+            get { return mb; }
+            set { mb = value; }
+        }
+
+        public int Ma
+        {
+            get { return ma; }
+            set { ma = value; }
+        }
+
+        public double CV()
+        {
+            return Ma + Mb + Mc;
+        }
+
+        public double DT()
+        {
+            double nuaCV = (CV()/2);
+            return Math.Sqrt(nuaCV * (nuaCV - Ma) * (nuaCV - Mb) * (nuaCV - Mc));
+        }
+
+        public TamGiac() { }
+
+        public void KiemTra()
+        {
+            if (Ma + Mb < Mc || Ma + Mc < Mb || Mb + Mc < Ma)
+            {
+                //la tam giac
+                Console.WriteLine("day khong phai la tam giac ");
+            }
+            else if (Ma == Mb && Mb == Mc)
+            {
+                //tam giac deu
+                Console.WriteLine("day la tam giac deu");
+            }
+            else if (Ma == Mb || Ma == Mc || Mb == Mc) // 2 canh bat ky bang nhau
+            {
+                // tam giac can
+                Console.WriteLine("day la tam giac can");
+            }
+            else
+            {
+                // khong phai tam giac
+                Console.WriteLine("la  TAM GIAC ");
+            }
+        }
+
+    }
+}
