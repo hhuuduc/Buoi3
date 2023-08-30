67e7aff03e934b55259da0952ea1e0ece5ff49b4
A209PC28\Administrator <huynhhuuduc5031@gmail.com>
2023-08-30 14:02:00 +0700

    add SinhVien.cs

--- 2001207422_HuynhHuuDuc_Buoi03/Bai2/SinhVien.cs
+++ 2001207422_HuynhHuuDuc_Buoi03/Bai2/SinhVien.cs
@@ -8,5 +8,28 @@ namespace Bai2
 {
     class SinhVien
     {
+        private int maSV;
+        private string hoTenSV;
+        private float diemLT, diemTH;
+
+        public int getMaSV(int model)
+        {
+            return maSV;
+        }
+
+        public string getHoTenSV(string model)
+        {
+            return hoTenSV;
+        }
+
+        public float getDiemLT(float model)
+        {
+            return diemLT;
+        }
+
+        public float getDiemTH(float model)
+        {
+            return diemTH;
+        }
     }
 }
